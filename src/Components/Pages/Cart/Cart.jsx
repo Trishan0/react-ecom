9bd1ac834b0ccf25cb9a968fbfa022d98f2a9307
trishan <sanjanatrishan@gmail.com>
2025-12-30 18:06:19 +0530

    Refactor cart component to improve item display and update functionality

--- src/Components/Pages/Cart/Cart.jsx
+++ src/Components/Pages/Cart/Cart.jsx
@@ -1,61 +1,95 @@
-import {useContext} from 'react'
-import {ThemeContext} from '../../../context/ThemeContext'
-import {useCart} from '../../../context/CartContext'
-
+import { useContext } from "react";
+import { ThemeContext } from "../../../context/ThemeContext";
+import { useCart } from "../../../context/CartContext";
+import "./Cart.css";
 function Cart() {
-    const {theme, toggleTheme} = useContext(ThemeContext)
-    const {cartItems, setCartItems} = useCart()
+    const { theme, toggleTheme } = useContext(ThemeContext);
+    const { cartItems, setCartItems } = useCart();
 
     const themeStyles = {
-        backgroundColor: theme === 'light' ? '#FFF' : '#333',
-        color: theme === 'light' ? '#333' : '#FFF',
-        margin: '2rem',
-        padding: '2rem',
-    }
+        backgroundColor: theme === "light" ? "#FFF" : "#333",
+        color: theme === "light" ? "#333" : "#FFF",
+        margin: "2rem",
+        padding: "2rem",
+    };
 
     const removeFromCart = (productId) => {
-        const updatedCart = cartItems.filter(item => item.id !== productId)
-        setCartItems(updatedCart)
-    }
-    const updateCount = (productId, opt = 'plus') => {
-        const updatedCart = cartItems.map(item => {
+        const updatedCart = cartItems.filter((item) => item.id !== productId);
+        setCartItems(updatedCart);
+    };
+    const updateCount = (productId, opt = "plus") => {
+        const updatedCart = cartItems.map((item) => {
             if (item.id !== productId) return item;
 
-            if (opt === 'minus' && item.qty === 1) {
+            if (opt === "minus" && item.qty === 1) {
                 alert("Quantity can't be Zero");
-                return item
+                return item;
             }
             return {
-                ...item
-                , qty: opt === 'plus' ? item.qty + 1 : item.qty - 1
-            }
-        })
-        setCartItems(updatedCart)
-    }
+                ...item,
+                qty: opt === "plus" ? item.qty + 1 : item.qty - 1,
+            };
+        });
+        setCartItems(updatedCart);
+    };
+    const totalPrice = cartItems.reduce((acc, item) => {
+        return acc + item.price * item.qty;
+    }, 0);
 
     return (
-        <>
-            <div>Cart</div>
+        <div>
             <div style={themeStyles}>
                 <h2>Items in Cart ({cartItems.length})</h2>
                 <ul>
-                    {cartItems.map((item, index) => {
-                        return <li key={index}>{item.title} - {item.qty}
-                            <button style={{margin: '10px'}} onClick={() => updateCount(item.id, 'plus')}>Increase
-                            </button>
-                            <button style={{margin: '10px'}} disabled={item.qty === 1}
-                                    onClick={() => updateCount(item.id, 'minus')}>Decrease
-                            </button>
-                            <button onClick={() => removeFromCart(item.id)}>Remove</button>
-                        </li>
+                    {cartItems.map((item) => {
+                        return (
+                            <div className="item-top" key={item.id}>
+                                <li className="item-line">
+                                    <span>
+                                        {item.title} × {item.qty}
+                                    </span>
+                                    <span>{item.price * item.qty}$</span>
+
+                                    <div className="btn-section">
+                                        <button
+                                            className="btn btn-inc"
+                                            onClick={() =>
+                                                updateCount(item.id, "plus")
+                                            }
+                                        >
+                                            +
+                                        </button>
+
+                                        <button
+                                            className="btn btn-dec"
+                                            disabled={item.qty === 1}
+                                            onClick={() =>
+                                                updateCount(item.id, "minus")
+                                            }
+                                        >
+                                            −
+                                        </button>
+
+                                        <button
+                                            className="btn btn-remove"
+                                            onClick={() =>
+                                                removeFromCart(item.id)
+                                            }
+                                        >
+                                            Remove
+                                        </button>
+                                    </div>
+                                </li>
+                            </div>
+                        );
                     })}
                 </ul>
+                <p>Total Price: {totalPrice}$</p>
                 <p>The current theme is {theme}</p>
                 <button onClick={toggleTheme}>Toggle Theme</button>
             </div>
-        </>
-
-    )
+        </div>
+    );
 }
 
-export default Cart
+export default Cart;
